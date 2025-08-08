7637d49b4f47830b7fec3d58bb10da261f040749
anju.sreekumar <anjtoanj@gmail.com>
2025-08-08 21:46:47 +1200

    updated ProjectSpecificMethods.java

---
+++ src/main/java/com/framework/utils/Reporter.java
@@ -0,0 +1,21 @@
+package com.framework.utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.framework.seleniumapi.base.DriverInstance;
+
+public abstract class Reporter extends DriverInstance {
+
+    public  static ExtentReports extent;
+    public static final ThreadLocal<ExtentTest> parentTest = new ThreadLocal<>();
+    public static final ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+    public static final ThreadLocal<String> testName = new ThreadLocal<>();
+
+    private String fileName = "result.html";
+    private String pattern = "dd-MMM-yyyy HH-mm-ss";
+
+    public String testcaseName, testDescription, authors, category, dataFileName, dataFileType, excelFileName,sheetName;
+    public static String folderName = "";
+
+
+}
