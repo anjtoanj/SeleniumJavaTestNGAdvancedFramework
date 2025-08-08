75ba44d37dc27074809e8cfc81176087b7147a1f
anju.sreekumar <anjtoanj@gmail.com>
2025-08-08 21:31:11 +1200

    updated ProjectSpecificMethods.java

--- src/main/java/com/framework/testngapi/ProjectSpecificMethods.java
+++ src/main/java/com/framework/testngapi/ProjectSpecificMethods.java
@@ -12,26 +12,35 @@ import com.framework.utils.DataLibrary;
 public class ProjectSpecificMethods extends SeleniumBase {
 
     public static Properties prop;
-    public static String username, password ;
+
+    public static String username, password;
     public static String contactName;
 
-    @DataProvider(name ="fetchData", indices = 0)
-    public Object[][] fetchData() throws  IOException{
-        return DataLibrary.readExecelData(excelFileName);
+    @DataProvider(name = "fetchData", indices = 0)
+    public Object[][] fetchData() throws IOException {
+        return DataLibrary.readExcelData(excelFileName);
     }
-    @BeforeMethod
-    public  void precondition() throws IOException{
 
+    @BeforeMethod
+    public void preCondition() throws IOException {
+
+        //Loads the configuration file located in your resources folder.
+        // FileInputStream reads the contents of config.properties into the Java program.
         FileInputStream config = new FileInputStream("./src/main/resources/config.properties");
         prop = new Properties();
         prop.load(config);
+//		Initializes the Properties object.
+//		Loads the key-value pairs from the file (like browser=chrome, url=https://example.com, etc.) into memory.
         startApp("chrome", false, prop.getProperty("url"));
+//        setNode();
         username = prop.getProperty("username");
         password = prop.getProperty("password");
-    }
 
+    }
 
     public void postCondition() {
         close();
+
     }
 }
+
